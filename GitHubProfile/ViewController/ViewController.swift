976678b24faf33c85955343a32027f93c00c8143
정유진 <jeong-yujin@jeong-yujin-ui-MacBookAir.local>
2024-04-17 19:38:49 +0900

    [Refactor]: extracting setProfielData method

--- GitHubProfile/ViewController/ViewController.swift
+++ GitHubProfile/ViewController/ViewController.swift
@@ -51,19 +51,23 @@ class ViewController: UIViewController {
             switch result {
             case .success(let userProfile):
                 DispatchQueue.main.async {
-                    if let profileImageUrl = URL(string: userProfile.avatarUrl) {
-                        self?.profileImage.kf.setImage(with: profileImageUrl)
-                    }
-                    self?.userId.text = username
-                    self?.userName.text = userProfile.name
-                    self?.userLocation.text = userProfile.location
-                    self?.followersCount.text = "✨Followers: \(userProfile.followers)"
-                    self?.followingCount.text = "✨Following: \(userProfile.following)"
+                    self?.setProfileData(userProfile)
                 }
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+    func setProfileData(_ userProfile: UserProfile) {
+        if let profileImageUrl = URL(string: userProfile.avatarUrl) {
+            self.profileImage.kf.setImage(with: profileImageUrl)
+        }
+        self.userId.text = userProfile.login
+        self.userName.text = userProfile.name
+        self.userLocation.text = userProfile.location
+        self.followersCount.text = "✨Followers: \(userProfile.followers)"
+        self.followingCount.text = "✨Following: \(userProfile.following)"
     }
     
     // MARK: - Repository 가져오기
@@ -89,7 +93,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryTableViewCell",  for: indexPath) as! RepositoryTableViewCell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryTableViewCell",  for: indexPath) as? RepositoryTableViewCell else { return UITableViewCell() }
         
         cell.setData(repositories[indexPath.row])
         return cell
